a12087e0449b01cf58709f19c398e4c77ca37d2a
Jader Machado <jadermachado@live.com>
2019-08-15 10:55:56 -0300

    Aula sobre operadores Mat e Comp

---
+++ RubyBasic/A3_Operadores.rb
@@ -0,0 +1,30 @@
+# 
+# Num1 = 0
+# Num2 = 0
+# Total = 0
+
+# puts 'Informe o Valor 1'
+# Num1 = gets.chomp.to_i
+
+# puts 'Informe o Valor 2'
+# Num2 = gets.chomp.to_i
+# #Operadores Mat
+# #Total = Num1 + Num2
+# #Total = Num1 - Num2
+# #Total = Num1 * Num2
+# Total = Num1 / Num2
+# puts Total
+
+# #Operadores Logicos
+v1=12
+v2=12
+
+puts v1.eql?(v2)
+
+#igual ==
+# igual  .eql?()
+# diferente !=
+# Maior >
+# Menor <
+# Maior igual >=
+# Menor Igual <=
