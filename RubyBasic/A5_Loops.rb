b713b7e8daa7c826c1607752d17d8d2f9e34fb2e
Jader Machado <jadermachado@live.com>
2019-08-15 11:28:31 -0300

    Aula de laços de repetição

---
+++ RubyBasic/A5_Loops.rb
@@ -0,0 +1,24 @@
+
+##TIMES
+# 5.times do |i|   
+#     puts 'Repetindo a msg' + (i+1).to_s+ + ' vezes' 
+# end
+
+##WHILE 
+# inicial = 0
+# while inicial <= 10 do
+#     puts 'Repetindo a msg' + (inicial+1).to_s+ + ' vezes' 
+#     inicial=inicial+1
+# end
+
+##FOR
+# for item in (0...10)
+#     puts 'Repetindo a msg' + (item+1).to_s+ + ' vezes' 
+# end
+
+##ARRAYS
+vingadores = ['ironman', 'hulk', 'homenaranha', 'thor']
+
+vingadores.each do |v|
+    puts v
+end
