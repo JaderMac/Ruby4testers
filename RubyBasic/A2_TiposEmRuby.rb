e742470415dbc6b9e6fae36263aaca9b387f6cdd
Jader Machado <jadermachado@live.com>
2019-08-15 10:37:32 -0300

    Aula sobre tipos em ruby

---
+++ RubyBasic/A2_TiposEmRuby.rb
@@ -0,0 +1,18 @@
+
+produto = 'Mac Book Air'
+preço =  534.99
+qtd = 10
+disponivel = true
+
+puts produto 
+puts preço
+puts qtd
+puts disponivel
+
+qtd = '10'
+
+puts produto.class 
+puts preço.class
+puts qtd.class
+puts disponivel.class
+
